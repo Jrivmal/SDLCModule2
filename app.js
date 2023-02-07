fb742e006b2ff4d5a84a268c4b9cee4abeea381c
Janet Malavet <J_rm78@yahoo.com>
2023-02-06 19:35:25 -0500

    added new java code

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class Module2Poem {
-
-	public static void main (String []args) throws FileNotFoundException {
-		File file = new File("C:\\Users\\j_rm7\\eclipse-workspace\\SoftwareDevelopment\\src\\module2poem\\RavenPoem");
-		Scanner scan = new Scanner(file);
-		
-		scan.useDelimiter("[^A-Za-z]+");
-		
-		while(scan.hasNextLine()) {
-		
-			System.out.println(scan.next());
-			
-		}
-		
-	}
+    public static void main(String[] args) throws IOException {
+           File file = new File("C:\\Users\\j_rm7\\eclipse-workspace\\SoftwareDevelopment\\src\\module2poem\\RavenPoem");
+            BufferedReader br = new BufferedReader(new FileReader(file));
+        String line;
+        Map<String, Long> counts = new HashMap<>();
+        while ((line = br.readLine()) != null) {
+                    String[] words = line.split("[\\s.;,?:!()\"]+");
+            for (String word : words) {
+                word = word.trim();
+                if (word.length() > 0) {
+                    if (counts.containsKey(word)) {
+                        counts.put(word, counts.get(word) + 1);
+                    } else {
+                        counts.put(word, 1L);
+                    }
+                }
+            }
+        }
+        for (Map.Entry<String, Long> entry : counts.entrySet()) {
+        	
+            System.out.println(entry.getKey() + " = " + entry.getValue());
+        }
+      
+    }
 }
